ce25e801f8af2dbedbbb227c0545ca32443d4679
Muchiri <smuchiri@quantrica.com>
2021-04-04 00:30:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href="/">Store made with SOKO</a>
+        
       </header>
+      <main>
+        <div className="target">
+        <p>Target
+        </p>
+        <p>Cham Towers, PLOT 12 Nkuruma Rd, Kampala, Ug</p>
+        </div>
+        products
+      </main>
     </div>
   );
 }
